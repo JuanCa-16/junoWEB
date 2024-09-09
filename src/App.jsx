9ef628d519e56ddfced035aa3dcfeeaecb33a138
JuanCa-16 <juancamilohg162003@gmail.com>
2024-09-09 11:17:12 -0500

    Prueba de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,17 @@ function MainLayout({ closeMenu, setCloseMenu }) {
   );
 }
 
+// Layout para Login y Registro sin Sidebar
+function AuthLayout() {
+  return (
+    <div className="auth-container">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Registro />} />
+      </Routes>
+    </div>
+  );
+}
 
 function App() {
   const [closeMenu, setCloseMenu] = useState(() => {
@@ -38,8 +49,7 @@ function App() {
     <Router>
       <Routes>
         {/* Rutas de autenticación sin Sidebar */}
-        <Route path="/login" element={<div className="auth-container"><Login /></div>} />
-        <Route path="/registro" element={<div className="auth-container"><Registro/></div>} />
+        <Route path="/usuario/*" element={<AuthLayout/>} />
 
         {/* Rutas principales con Sidebar */}
         <Route path="/*" element={<MainLayout closeMenu={closeMenu} setCloseMenu={setCloseMenu} />} />
